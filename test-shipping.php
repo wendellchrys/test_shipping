5a05dd7df17a8fc8e82d39ed34ff4c36282ef2a2
wendellchrys <wendell_chrys@hotmail.com>
2019-07-15 15:29:00 -0300

    Update test-shipping.php

--- test-shipping.php
+++ test-shipping.php
@@ -31,28 +31,79 @@ if ( ! class_exists( 'Test_Shipping_Br' ) ) {
 
             add_action( 'woocommerce_update_options_shipping_' . $this->id, array( $this, 'process_admin_options' ) );
         }
-
-        public function calculate_shipping( $packages = array() ) {
-
-            $array = WC()->cart->needs_shipping();                  
-            $postcode = $array[0]['destination']['postcode'];
-			$cost = 0;
-			if ($cost == 0){
-				$semfrete = "Insira seu cep para calcular o frete";
-			}
-			else {
-				$semfrete = $this->title;
-			}
-            if ($postcode >= 75960000 && $postcode <= 75969999)
-            {
-                $cost = 17;
-				
-
+        
+         public function get_cart_volume(){
+                // Initializing variables
+                $volume = $rate = 0;
+            
+                // Get the dimetion unit set in Woocommerce
+                $dimension_unit = get_option( 'woocommerce_dimension_unit' );
+            
+                // Calculate the rate to be applied for volume in m3
+                if ( $dimension_unit == 'mm' ) {
+                    $rate = pow(10, 9);
+                } elseif ( $dimension_unit == 'cm' ) {
+                    $rate = pow(10, 6);
+                } elseif ( $dimension_unit == 'm' ) {
+                    $rate = 1;
+                }
+            
+                if( $rate == 0 ) return false; // Exit
+            
+                // Loop through cart items
+                foreach(WC()->cart->get_cart() as $cart_item) { 
+                    // Get an instance of the WC_Product object and cart quantity
+                    $product = $cart_item['data'];
+                    $qty     = $cart_item['quantity'];
+            
+                    // Get product dimensions  
+                    $length = $product->get_length();
+                    $width  = $product->get_width();
+                    $height = $product->get_height();
+            
+                    // Calculations a item level
+                    $volume += $length * $width * $height * $qty;
+                } 
+                return $volume / $rate;
             }
+            
+            $volume = get_cart_volume();
+            
+
+        public function calculate_shipping( $package = array() ) {
+            
+		// Check if valid to be calculeted.
+		if ( '' === $package['destination']['postcode'] || 'BR' !== $package['destination']['country'] ) {
+			return;
+		}
+                        
+		$postcode = $package['destination']['postcode'];
+		$state = $package['destination']['state'];
+		
+		$postcode = str_replace('-', '', $postcode);
+		$postcode = intval($postcode);
+		$cost = 0;
+            
+		if ($postcode >= 75960000 && $postcode <= 75969999 && $state == 'GO')
+		{
+			$cost = 17;
+			$titleShipping = 'Test X:';
+		}
+		
+		elseif  ($volume > 100)
+		{
+			$cost = 100;
+			$titleShipping = 'Test Y:';
+		}
+            
+            if ($cost == 0){
+		    $titleShipping = 'Test Z';
+	    }
+               
 
             $rate = array(
                 'id' => $this->id,
-                'label' => $semfrete,
+                'label' => $titleShipping,
                 'cost' => round($cost,2),
                 'calc_tax' => 'per_order'
             );
